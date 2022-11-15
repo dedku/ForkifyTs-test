f08da7728f2630a7dafc63b354ef71ffa041bd72
Patryk Sz <szabat.patryk@gmail.com>
2022-11-15 20:43:10 +0100

    Rendering the Recipe

--- src/ts/controller.test.ts
+++ src/ts/controller.test.ts
@@ -1,7 +1,16 @@
-import { expect, describe, it } from "vitest"
+import { expect, describe, it, beforeAll } from "vitest"
+import { renderSpinner, showRecipe, FORKIFY_LINK, recipeContainer } from './controller'
+
+// TODO: make DOM element work in tests
 
 describe('test', () => {
-    it("test some", () => {
-
+    beforeAll(() => {
+        Object.defineProperty(global, 'document', {});
+    })
+    it("test some", async () => {
+        const div = document.createElement('div');
+        div.classList.add('recipe');
+        document.body.appendChild(div);
+        await expect(renderSpinner(recipeContainer)).toBeCalled()
     })
 })
--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -12,25 +12,138 @@ type recipeType = {
   id: string,
   title: string,
   publisher: string,
-  sourceUrl: string,
-  image: string,
+  source_url: string,
+  image_url: string,
   servings: number,
-  cookingTime: number,
-  ingredients: object[]
+  cooking_time: number,
+  ingredients: ingridientsType[]
 }
 
+type ingridientsType = {
+  quantity: number,
+  unit: string,
+  description: string,
+}
 const FORKIFY_LINK = `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${import.meta.env.FORKIFY_API_KEY}`
 
-const showRecipe = async function (link: string): Promise<recipeType> {
+
+const recipeContainer = document.querySelector('.recipe') as HTMLBodyElement
+
+const renderSpinner = function (parentEl: HTMLBodyElement): void {
+  const markup = `
+  <div class="spinner">
+    <svg>
+      <use href="src/img/icons.svg#icon-loader"></use>
+    </svg>
+  </div>`
+
+  parentEl.innerHTML = ''
+  parentEl.insertAdjacentHTML('afterbegin', markup)
+}
+
+const showRecipe = async function (link: string): Promise<void> {
   try {
+    // 1. Load recipe
+    renderSpinner(recipeContainer)
+
     const res: Response = await fetch(link)
 
     const data: resDataType = await res.json()
+
     if (!res.ok) throw new Error(`${data.message} (${res.status})`)
 
     let recipe = data.data.recipe
-    console.log(recipe)
-    return recipe
+
+    // 2. Rendering Recipe
+    const markup = `
+    <figure class="recipe__fig">
+      <img src="${recipe.image_url}" alt="${recipe.title}" crossOrigin = "anonymous"  class="recipe__img" />
+      <h1 class="recipe__title">
+        <span>${recipe.title}</span>
+      </h1>
+    </figure>
+
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="src/img/icons.svg#icon-clock"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${recipe.cooking_time}</span>
+        <span class="recipe__info-text">minutes</span>
+      </div>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="src/img/icons.svg#icon-users"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+        <span class="recipe__info-text">servings</span>
+
+        <div class="recipe__info-buttons">
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="src/img/icons.svg#icon-minus-circle"></use>
+            </svg>
+          </button>
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="src/img/icons.svg#icon-plus-circle"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div class="recipe__user-generated">
+        <svg>
+          <use href="src/img/icons.svg#icon-user"></use>
+        </svg>
+      </div>
+      <button class="btn--round">
+        <svg class="">
+          <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+        </svg>
+      </button>
+    </div>
+
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
+        ${recipe.ingredients.map(ing => {
+      return `
+        <li class="recipe__ingredient">
+          <svg class="recipe__icon">
+            <use href="src/img/icons.svg#icon-check"></use>
+          </svg>
+          <div class="recipe__quantity">${ing.quantity}</div>
+          <div class="recipe__description">
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.description}
+          </div>
+        </li>
+        `}).join('')}
+
+      </ul>
+    </div>
+
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+        directions at their website.
+      </p>
+      <a
+        class="btn--small recipe__btn"
+        href="${recipe.source_url}"
+        target="_blank"
+      >
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="src/img/icons.svg#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>`
+    recipeContainer.innerHTML = ''
+    recipeContainer.insertAdjacentHTML('afterbegin', markup)
   } catch (err) {
     throw (err)
   }
@@ -38,4 +151,4 @@ const showRecipe = async function (link: string): Promise<recipeType> {
 
 showRecipe(FORKIFY_LINK)
 
-export { FORKIFY_LINK, showRecipe }
+export { FORKIFY_LINK, showRecipe, renderSpinner, recipeContainer }
