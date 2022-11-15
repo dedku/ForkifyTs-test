3a7116584a03d1de17bb04eb1cdee7f4ceb8e436
Patryk Sz <szabat.patryk@gmail.com>
2022-11-15 18:20:45 +0100

    Loading recipe from API

--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -1,14 +1,41 @@
-const recipeContainer = document.querySelector('.recipe') as HTMLBodyElement;
+type resDataType = {
+  status: string,
+  data: recipeObjectType,
+  message?: string
+}
 
-const timeout = function (s: any) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+type recipeObjectType = {
+  recipe: recipeType
+}
 
+type recipeType = {
+  id: string,
+  title: string,
+  publisher: string,
+  sourceUrl: string,
+  image: string,
+  servings: number,
+  cookingTime: number,
+  ingredients: object[]
+}
 
-// https://forkify-api.herokuapp.com/v2
+const FORKIFY_LINK = `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${import.meta.env.FORKIFY_API_KEY}`
 
-///////////////////////////////////////
+const showRecipe = async function (link: string): Promise<recipeType> {
+  try {
+    const res: Response = await fetch(link)
+
+    const data: resDataType = await res.json()
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    let recipe = data.data.recipe
+    console.log(recipe)
+    return recipe
+  } catch (err) {
+    throw (err)
+  }
+}
+
+showRecipe(FORKIFY_LINK)
+
+export { FORKIFY_LINK, showRecipe }
